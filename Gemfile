078efb5416098e9cda32c9608e2d707f54d002f3
Alex Churchill <alex.churchill@cantab.net>
2010-11-01 00:15:01 +0000

    Details pages working with Markdown

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.0'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'will_paginate', '3.0.pre2'
+gem 'maruku'
 
 group :development do
   gem 'annotate-models', '1.0.4'
--- app/models/details_page.rb
+++ app/models/details_page.rb
@@ -1,5 +1,7 @@
 class DetailsPage < ActiveRecord::Base
   belongs_to :cardset
+  validates_presence_of :title
+  # validates_length_of :title, :within => 1..140
 
   def recency  # For a details page, its order in recency is when it was updated
     updated_at
