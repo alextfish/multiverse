2c2e3ecd7f31f388d3f9943ca3b825956c80414a
Alex Churchill <alex.churchill@cantab.net>
2010-11-06 12:33:50 +0000

    Put back the comments->cardset migration

--- db/disabled_migrate/20101103221620_add_cardset_id_to_comment.rb
+++
@@ -1,9 +0,0 @@
-class AddCardsetIdToComment < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :cardset_id, :integer
-  end
-
-  def self.down
-    remove_column :comments, :cardset_id
-  end
-end
