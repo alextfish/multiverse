b6ef1366ab4e21ed52abb560de054047be99f1fd
Alex Churchill <alex.churchill@cantab.net>
2010-11-22 23:53:29 +0000

    New infrastructure for permissions; nicer timestamps

--- db/migrate/20101122224800_remove_colour_from_cards.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColourFromCards < ActiveRecord::Migration
-  def self.up
-    remove_column :cards, :colour
-  end
-
-  def self.down
-    add_column :cards, :colour, :string
-  end
-end
