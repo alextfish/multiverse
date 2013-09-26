7e1df70e11c10e273813c43169b1e6f80cc84e79
alextfish <alex.churchill@cantab.net>
2013-09-26 10:30:48 +0100

    Tweak assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Multiverse::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or Heroku will already do this
   config.serve_static_assets = false
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
