ad649d41323b0f5fd0b6c752d13fed1bee14b5e8
Alex Churchill <alex.churchill@cantab.net>
2015-02-14 21:02:19 +0000

    Revert "Bugfix to searching for details pages, not that anyone ever would"
    This reverts to commit b32506ff2d99335e60b0323595ad81cca78bb27d.
    Seeing crazy crashes on Heroku with my latest updates... don't think this
    will help, but trying it anyway.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -196,7 +196,7 @@ class SearchesController < ApplicationController
                     permission_to? :view, card.cardset
                   end
         when :details_pages
-          out = DetailsPage.includes([ { :cardset => :configuration }]).where( condition ).select do |dp|
+          out = DetailsPage.includes([ :card, { :cardset => :configuration }]).where( condition ).select do |dp|
                     permission_to? :view, dp.cardset
                   end
         when :comments
